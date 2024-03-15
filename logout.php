3d27d18982d438b7aac3414d8a61a8c24814be39
xoJude <judeggn@gmail.com>
2024-03-15 12:53:49 +0100

    feat: new files

--- logout.php
+++ logout.php
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 <?php
     session_start();
     session_destroy();
     header("Location: connexion.php");
 ?>
-=======
-<?php
-    session_start();
-    session_destroy();
-    header("Location: connexion.php");
-?>
->>>>>>> main
