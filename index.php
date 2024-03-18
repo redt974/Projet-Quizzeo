387c78a51b77c62c2c74c942e5d2366a395cad51
Emrys <emrysagboton@gmail.com>
2024-03-18 16:20:33 +0200

    index.php slider

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
     <link rel="icon" href='./assets/quizzeo.ico' />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./style/index.css">
 </head>
+
 <body>
     <?php
         session_start();
@@ -16,10 +21,11 @@
             header('location: connexion.php');
         }
         include './components/header.php';
-
-        echo '<video id="background-video" autoplay loop muted>
-                <source src="./assets/background6.mp4">
-              </video>';
+    ?>
+    <video id="background-video" autoplay loop muted>
+        <source src="./assets/background6.mp4">
+    </video>
+    <?php
         if ($_SESSION['role'] == 'admin'){
             // Ouvrir le fichier CSV en lecture
             $file = fopen('utilisateurs.csv', 'r');
@@ -56,113 +62,138 @@
         }
     ?>
     <?php if ($_SESSION['role'] == 'school' || $_SESSION['role'] == 'company') : ?>
-        <a class="quiz" href="quiz.php">Add Quiz</a>
+    <a class="quiz" href="quiz.php">Add Quiz</a>
     <?php endif; ?>
     <?php if ($_SESSION['role'] == 'user' || $_SESSION['role'] == 'admin') : ?>
-        <!-- Slider des quiz crées -->
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <?php
-                    // Ouvrir le fichier CSV en lecture
-                    $file = fopen('user_quiz.csv', 'r');
+    <div class="container">
+        <div class="slide">
+            <?php
+                // Tableau des URLs des images
+                $image_urls = [
+                    "https://i.ibb.co/qCkd9jS/img1.jpg",
+                    "https://i.ibb.co/jrRb11q/img2.jpg",
+                    "https://i.ibb.co/NSwVv8D/img3.jpg",
+                    "https://i.ibb.co/Bq4Q0M8/img4.jpg",
+                    "https://img.freepik.com/premium-photo/lakes_972708-78.jpg?size=626&ext=jpg&ga=GA1.1.735520172.1710288000&semt=ais"
+                ];
 
-                    // Compteur pour suivre l'index de la diapositive
-                    $index = 0;
+                // Ouvrir le fichier CSV en lecture
+                $file = fopen('user_quiz.csv', 'r');
 
-                    // Vérifier si le fichier contient des données 
-                    if (($row = fgetcsv($file)) !== false) {
-                        // Vérifier si la ligne suivante contient des données
-                        while (($row = fgetcsv($file)) !== false) {
-                            if ($row !== null) {
-                                // Incrémenter l'index de la diapositive
-                                $index++;
+                // Index de l'image à utiliser
+                $image_index = 0;
 
-                                // Afficher l'indicateur
-                                $active_class = ($index === 1) ? 'active' : '';
-                                echo "<button type='button' data-bs-target='#carouselExampleCaptions' data-bs-slide-to='$index' class='$active_class' aria-current='true' aria-label='Slide $index'></button>";
-                            }
+                // Vérifier si le fichier contient des données 
+                fgetcsv($file);
+
+                // Boucler à travers les lignes du fichier CSV
+                while (($row = fgetcsv($file)) !== false) {
+                    if ($row !== null) {
+                        // Récupérer l'URL de l'image ou utiliser l'URL correspondante dans le tableau $image_urls
+                        $image_url = !empty($row[4]) ? $row[4] : $image_urls[$image_index];
+
+                        // Afficher la diapositive du carousel avec les données du fichier CSV
+                        echo "<div class='item' style='background-image: url(" . $image_url . ");'>
+                                <div class='content'>
+                                    <div class='name'>" . $row[2] . "</div>
+                                    <div class='des'>" . $row[3] . "</div>";
+                        if ($_SESSION['role'] == 'user'){
+                            echo "<button class='game' onclick='startGame(" . $row[0] . ")'>Start</button>";
+                        }
+                        echo "</div>
+                            </div>";
+
+                        // Incrémenter l'index pour la prochaine itération
+                        $image_index++;
+                        // Si l'index dépasse la taille du tableau, réinitialiser à zéro
+                        if ($image_index >= count($image_urls)) {
+                            $image_index = 0;
                         }
                     }
-                    // Fermer le fichier
-                    fclose($file);
-                ?>
-            </div>
-            <div class="carousel-inner">
-                <?php
-                    // Ouvrir le fichier CSV en lecture
-                    $file = fopen('user_quiz.csv', 'r');
+                }
 
-                    // Tableau contenant les noms de fichiers des images
-                    $images = array(
-                        "space.jpg",
-                        "world.jpg",
-                        "ucl.jpg",
-                        "dbz.jpg",
-                        "cinema.jpg",
-                        "logo.webp",
-                        "geo.jpg",
-                        "animaux.png"
-                    );
-
-                    // Compteur pour suivre l'index de la diapositive
-                    $index = 0;
-
-                    // Vérifier si le fichier contient des données 
-                    fgetcsv($file);
-                        // Vérifier si la ligne suivante contient des données
-                        while (($row = fgetcsv($file)) !== false) {
-                            if ($row !== null) {
-                                // Incrémenter l'index de la diapositive
-                                $index++;
-
-                                // Vérifier si c'est la première diapositive, pour la marquer comme active
-                                $active_class = ($index === 1) ? 'active' : '';
-
-                                // Sélectionner l'image correspondante à l'index actuel
-                                $image = isset($images[$index - 1]) ? $images[$index - 1] : '';
-
-                                // Afficher la diapositive du carousel avec les données du fichier CSV
-                                echo "<div class='carousel-item $active_class'>
-                                        <img src='./assets/$image' class='d-block w-100' alt='Slide Image'>
-                                        <div class='carousel-caption d-none d-md-block'>
-                                            <!-- Utilisez les données du fichier CSV pour le titre et la description -->
-                                            <h5>" . $row[2] . "</h5>
-                                            <p>" . $row[3] . "</p>";
-                                            if ($_SESSION['role'] == 'user'){
-                                                echo "<button class='quiz game' onclick='startGame(" . $row[0] . ")'>Start</button>";
-                                            }
-                                        echo "</div>
-                                    </div>";
-                            }
-                        }
-
-                    // Fermer le fichier
-                    fclose($file);
-                ?>
-                <?php if ($_SESSION['role'] == 'user') : ?>
-                <!-- Formulaire masqué pour envoyer les informations du quiz en méthode post -->
-                <form id="gameForm" action="game.php" method="post" style="display: none;">
-                    <input type="hidden" name="quiz_id" id="quiz_id">
-                    <input type="hidden" name="user_id" value="<?php echo $_SESSION['id']; ?>">
-                </form>
-                <script>
-                    function startGame(quizId) {
-                        document.getElementById('quiz_id').value = quizId;
-                        document.getElementById('gameForm').submit();
-                    }
-                </script>
-                <?php endif; ?>
-            </div>
-            <!-- Boutons de contrôle -->
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
+                // Fermer le fichier
+                fclose($file);
+            ?>
         </div>
+        <div class="button">
+            <button class="prev"><i class="fa-solid fa-arrow-left"></i></button>
+            <button class="next"><i class="fa-solid fa-arrow-right"></i></button>
+        </div>
+    </div>
+    <?php if ($_SESSION['role'] == 'user') : ?>
+        <!-- Formulaire masqué pour envoyer les informations du quiz en méthode post -->
+        <form id="gameForm" action="game.php" method="post" style="display: none;">
+            <input type="hidden" name="quiz_id" id="quiz_id">
+            <input type="hidden" name="user_id" value="<?php echo $_SESSION['id']; ?>">
+        </form>
+        <script>
+            function startGame(quizId) {
+                document.getElementById('quiz_id').value = quizId;
+                document.getElementById('gameForm').submit();
+            }
+        </script>
+    <?php endif; ?>
+    <script>
+        let next = document.querySelector('.next')
+        let prev = document.querySelector('.prev')
+
+        next.addEventListener('click', function () {
+            let items = document.querySelectorAll('.item')
+            document.querySelector('.slide').appendChild(items[0])
+        })
+
+        prev.addEventListener('click', function () {
+            let items = document.querySelectorAll('.item')
+            document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+        })
+    </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <?php if ($_SESSION['role'] == 'user') : ?>
+    <!-- Formulaire masqué pour envoyer les informations du quiz en méthode post -->
+    <form id="gameForm" action="game.php" method="post" style="display: none;">
+        <input type="hidden" name="quiz_id" id="quiz_id">
+        <input type="hidden" name="user_id" value="<?php echo $_SESSION['id']; ?>">
+    </form>
+    <script>
+        function startGame(quizId) {
+            document.getElementById('quiz_id').value = quizId;
+            document.getElementById('gameForm').submit();
+        }
+    </script>
+    <?php endif; ?>
+
     <?php endif; ?>
 </body>
+
 </html>
