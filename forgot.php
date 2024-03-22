f98fdb7d5f96444a4d5f8004c4c6890e6696f933
redt974 <t.vandier@ecole-ipssi.net>
2024-03-22 01:50:51 +0100

    feat: add favicon link

--- forgot.php
+++ forgot.php
@@ -13,6 +13,7 @@ include './components/header.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modifier le mot de passe</title>
+    <link rel="icon" href='./assets/quizzeo.ico' />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="./style/forgot.css">
 </head>
